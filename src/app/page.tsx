939ce8eea7c2e77f684ada049420062aee0d9b10
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-10 15:22:53 +0300

    Add NavigationHeader component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@
 
 import React from 'react';
 import Layout from '@/components/Layout';
+import NavigationHeader from '@/components/NavigationHeader';
 
 export default function HomePage() {
   return (
     <div className="flex min-h-screen flex-col">
+      <NavigationHeader />
+      
       <Layout />
       
       <main className="flex-1 container mx-auto px-4 py-8">
